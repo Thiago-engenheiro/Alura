76df84bcc3e262a7c9f5292e8b22474918d313a3
Thiago-engenheiro <thiago.principal11@outlook.com.br>
2024-08-04 11:34:57 -0300

    update
    - Atividade pronta !

--- Desenvolva aplicações Web com JavaScript/JavaScript manipulando elementos no DOM/Fokus-projeto-base/script.js
+++ Desenvolva aplicações Web com JavaScript/JavaScript manipulando elementos no DOM/Fokus-projeto-base/script.js
@@ -1,4 +1,3 @@
-
 const pagina = document.querySelector('html') 
 
 const botao01 = document.querySelector('.app__card-button--foco');
@@ -12,6 +11,22 @@ const texto = document.querySelector('.app__title');
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const musica = new Audio('sons/luna-rise-part-one.mp3');
 musica.loop = true;
+
+const mostraTempo = document.querySelector ('#timer');
+
+const temporizadorInput = document.querySelector('#start-pause');
+let tempoDecorrido = 1500;
+let intervaloId = null;
+
+const play = new Audio('sons/play.wav');
+const pausar = new Audio('sons/pause.mp3');
+const fim = new Audio('sons/beep.mp3');
+
+const iniciarOuPausarBt = document.querySelector('#start-pause span')
+const iniciarOuPausarImagem = document.querySelector('#start-pause .app__card-primary-butto-icon')
+
+iniciarOuPausarImagem.setAttribute('src', `./imagens/play_arrow.png`)
+mostrarTempo()
 
 musicaFocoInput.addEventListener('change', () => {
 
@@ -64,22 +79,37 @@ function alterarContexto(contexto) {
 
         case "foco" :
 
+            zerar()
+          
             texto.innerHTML = ` Otimize sua produtividade,<br>
             <strong class="app__title-strong">mergulhe no que importa.</strong>`
+
+            tempoDecorrido = 1500;
+            mostrarTempo()
 
         break;
 
         case "descanso-curto" :
 
+            zerar()
+           
             texto.innerHTML = ` Que tal dar uma respirada?,<br>
             <strong class="app__title-strong">Faça uma pausa curta!.</strong>`
+
+            tempoDecorrido = 600;
+            mostrarTempo()
 
         break;
 
         case "descanso-longo" :
 
+            zerar()
+            
             texto.innerHTML = ` Hora de voltar à superfície.,<br>
             <strong class="app__title-strong">Faça uma pausa longa..</strong>`
+
+            tempoDecorrido = 900;
+            mostrarTempo()
 
         break;
 
@@ -90,4 +120,59 @@ function alterarContexto(contexto) {
     }
 
 }
+
+const temporizador = () => {
+
+    if (tempoDecorrido <= 0) {
+
+        fim.play()
+        alert ("o tempo do foco acabou !")
+        zerar()
+        return
+
+    }
+
+    mostrarTempo()
+    tempoDecorrido -= 1
+
+}
+
+temporizadorInput.addEventListener('click', iniciar);
+
+function iniciar() {
+
+    if(intervaloId){
+
+        pausar.play()
+        zerar()
+        return
+
+    }
+
+    play.play()
+    intervaloId = setInterval(temporizador, 1000)
+    iniciarOuPausarImagem.setAttribute('src', `./imagens/play_arrow.png`)
+    iniciarOuPausarImagem.setAttribute('src', `./imagens/pause.png`)
+    iniciarOuPausarBt.textContent = "pausar"
+
+}
+
+function zerar() {
+
+    iniciarOuPausarImagem.setAttribute('src', `./imagens/play_arrow.png`)
+    iniciarOuPausarBt.textContent = "começar"
+    clearInterval(intervaloId) 
+    intervaloId = null
+
+}
+
+function mostrarTempo() {
+
+    const tempo = new Date(tempoDecorrido * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'})
+    mostraTempo.innerHTML = `${tempoFormatado}`
+
+    
+}
+
 
